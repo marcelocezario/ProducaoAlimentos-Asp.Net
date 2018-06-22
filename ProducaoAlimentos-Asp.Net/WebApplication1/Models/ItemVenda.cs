4fd945ca6a38b773bdfa44a587d198e0f685893d
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-21 21:54:31 -0300

    Criando model ItemVenda

---
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/ItemVenda.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class ItemVenda
+    {
+        public int ItemVendaID { get; set; }
+        public double ValorUnitarioItem { get; set; }
+        public double ValorTotalItem { get; set; }
+        public double QtdeProduto { get; set; }
+
+        [ForeignKey("_LoteProduto")]
+        public int LoteProdutoID { get; set; }
+        public virtual LoteProduto _LoteProduto { get; set; }
+    }
+}

57b1746f1ce5b8da64470c77d035bcf16eae725c
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-21 21:28:22 -0300

    Criando model InsumoComposicaoProduto

---
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/InsumoComposicaoProduto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class InsumoComposicaoProduto
+    {
+        public int InsumoComposicaoProdutoID { get; set; }
+        public double QtdeInsumo { get; set; }
+
+        [ForeignKey("_Insumo")]
+        public int InsumoID { get; set; }
+        public virtual Insumo _Insumo { get; set; }
+    }
+}

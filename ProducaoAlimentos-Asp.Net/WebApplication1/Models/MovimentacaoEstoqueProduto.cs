be065db8f3b564b4312a8feb2674c8d6fe0a369f
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-21 21:52:24 -0300

    Criando model MovimentacaoEstoqueProduto

---
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/MovimentacaoEstoqueProduto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class MovimentacaoEstoqueProduto : MovimentacaoEstoque
+    {
+        [ForeignKey("_LoteProduto")]
+        public int LoteInsumoID { get; set; }
+        public virtual LoteProduto _LoteProduto { get; set; }
+    }
+}

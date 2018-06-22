f49392f74ad7d23a2da3177746221ca908e9b01c
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-21 21:44:04 -0300

    Criando model Lote

---
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/Lote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public abstract class Lote
+    {
+        public int ID { get; set; }
+        public double QtdeInicial { get; set; }
+        public double QtdeDisponivel { get; set; }
+        public double CustoMedio { get; set; }
+        public double CustoTotalInicial { get; set; }
+        public DateTime Validade { get; set; }
+    }
+}

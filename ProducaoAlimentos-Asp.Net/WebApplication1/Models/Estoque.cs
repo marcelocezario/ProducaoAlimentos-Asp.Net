b02ab628049b376ad833b149ecf4b3917dca926c
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-21 21:32:55 -0300

    Criando model Estoque

---
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/Estoque.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public abstract class Estoque
+    {
+        public int ID { get; set; }
+        public double QtdeTotalEstoque { get; set; }
+        public double CustoTotalEstoque { get; set; }
+    }
+}

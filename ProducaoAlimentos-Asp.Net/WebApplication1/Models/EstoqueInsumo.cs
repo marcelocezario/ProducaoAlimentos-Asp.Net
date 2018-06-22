70bcd2d41da717f80601d297f7dc693b27185a79
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-21 21:35:24 -0300

    Criando model EstoqueInsumo

---
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/EstoqueInsumo.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class EstoqueInsumo : Estoque
+    {
+        [ForeignKey("_Insumo")]
+        public int InsumoID { get; set; }
+        public virtual Insumo _Insumo { get; set; }
+    }
+}

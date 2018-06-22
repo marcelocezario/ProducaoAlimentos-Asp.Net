259aad19a7a5c9cc77758b089f9a1915dc1b28fb
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-21 21:22:55 -0300

    Criando model Cidade

---
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/Cidade.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class Cidade
+    {
+        public int CidadeID { get; set; }
+        public string Nome { get; set; }
+
+        [ForeignKey("_Estado")]
+        public int EstadoID { get; set; }
+        public virtual Estado _Estado { get; set; }
+    }
+}

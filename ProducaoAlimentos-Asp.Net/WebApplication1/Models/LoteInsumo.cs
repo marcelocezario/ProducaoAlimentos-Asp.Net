c0f286ccd3cda2102693bd08c22df521580d8688
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-21 21:39:49 -0300

    Criando model LoteInsumo

---
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/LoteInsumo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class LoteInsumo
+    {
+        public int LoteInsumoID { get; set; }
+        public DateTime DataCompra { get; set; }
+
+        [ForeignKey("_Insumo")]
+        public int InsumoID { get; set; }
+        public virtual Insumo _Insumo { get; set; }
+
+        [ForeignKey("_Marca")]
+        public int MarcaID { get; set; }
+        public virtual Marca _Marca { get; set; }
+
+        [ForeignKey("_Fornecedor")]
+        public int FornecedorID { get; set; }
+        public virtual Fornecedor _Fornecedor { get; set; }
+    }
+}

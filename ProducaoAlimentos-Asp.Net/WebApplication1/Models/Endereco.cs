bf1b740f7137caaabc914ee3678f161715950d17
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-21 21:24:49 -0300

    Criando model Endereco

---
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/Endereco.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class Endereco
+    {
+        public int EnderecoID { get; set; }
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cep { get; set; }
+
+        [ForeignKey("_Cidade")]
+        public int CidadeID { get; set; }
+        public virtual Cidade _Cidade { get; set; }
+    }
+}

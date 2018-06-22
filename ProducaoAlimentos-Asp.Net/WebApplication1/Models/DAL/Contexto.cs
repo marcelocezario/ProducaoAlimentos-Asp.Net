2645a52bfdefabbef9552cbf6431373199027ec0
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-21 22:10:07 -0300

    Criando classe Contexto e SeedDB

---
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/DAL/Contexto.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Entity;
+
+namespace WebApplication1.Models.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("strConn")
+        {
+            Database.SetInitializer(new SeedDB());
+        }
+
+        public DbSet<Cidade> Cidades { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Estado> Estados { get; set; }
+        public DbSet<EstoqueInsumo> EstoqueInsumos { get; set; }
+        public DbSet<EstoqueProduto> EstoqueProdutos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Insumo> Insumos { get; set; }
+        public DbSet<InsumoComposicaoProduto> InsumosComposicaoProdutos { get; set; }
+        public DbSet<ItemVenda> ItensVenda { get; set; }
+        public DbSet<LoteInsumo> LotesInsumos { get; set; }
+        public DbSet<LoteProduto> LotesProdutos { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<MovimentacaoEstoqueInsumo> MovimentacoesEstoqueInsumos { get; set; }
+        public DbSet<MovimentacaoEstoqueProduto> MovimentacoesEstoqueProdutos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<UnidadeDeMedida> UnidadesDeMedida { get; set; }
+        public DbSet<Venda> Vendas { get; set; }
+    }
+}
---
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/DAL/SeedDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.DAL
+{
+    //public class SeedDB : DropCreateDatabaseIfModelChanges<Contexto>
+    public class SeedDB : DropCreateDatabaseAlways<Contexto>
+    {
+    }
+}

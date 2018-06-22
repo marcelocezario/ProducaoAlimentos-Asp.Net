c5152c169dec52b4232cec69344ddd0a29df8b86
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-21 21:36:34 -0300

    Criando model Produto

---
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/EstoqueProduto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class EstoqueProduto : Estoque
+    {
+        [ForeignKey("_Produto")]
+        public int ProdutoID { get; set; }
+        public virtual Produto _Produto { get; set; }
+    }
+}

cb4376248543b36d368735805b8394beab526f7d
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-21 21:46:47 -0300

    Criando model LoteProduto

---
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/LoteProduto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public class LoteProduto : Lote
+    {
+        public DateTime DataProducao { get; set; }
+        public double ValorVendaUnitario { get; set; }
+
+        public virtual List<LoteInsumoProducao> _ItensInsumoProducao { get; set; }
+    }
+}

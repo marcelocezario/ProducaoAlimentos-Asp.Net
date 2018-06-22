d1e88b8f975799fcfdc161ac689e41eea430b544
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-21 21:16:13 -0300

    Criando model Insumo

---
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/Insumo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class Insumo
+    {
+        public int InsumoID { get; set; }
+        public string Nome { get; set; }
+        
+        [ForeignKey("_UnidadeDeMedida")]
+        public int UnidadeDeMedidaID { get; set; }
+        public virtual UnidadeDeMedida _UnidadeDeMedida { get; set; }
+    }
+}

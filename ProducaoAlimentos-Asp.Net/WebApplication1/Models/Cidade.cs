f08895071889a56d1cde18a4a7084f832c75137f
Marcelo Cezário <marcelocezario@gmail.com>
2018-06-28 17:50:41 -0300

    Ajustando View Index de Cidade

--- ProducaoAlimentos-Asp.Net/WebApplication1/Models/Cidade.cs
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/Cidade.cs
@@ -1,10 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication1.Models
 {
     public class Cidade
     {
+        [Display(Name = "Id")]
         public int CidadeID { get; set; }
+        [Display(Name = "Cidade")]
         public string Nome { get; set; }
 
         [ForeignKey("_Estado")]
--- ProducaoAlimentos-Asp.Net/WebApplication1/Models/Estado.cs
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/Estado.cs
@@ -1,8 +1,11 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class Estado
     {
         public int EstadoID { get; set; }
+        [Display(Name = "Estado")]
         public string Nome { get; set; }
         public string Sigla { get; set; }
     }

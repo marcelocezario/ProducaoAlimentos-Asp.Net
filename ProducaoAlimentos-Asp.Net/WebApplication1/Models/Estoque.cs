e76a55b8dd39b76ff514f5bf28ced00ce94a3f08
Marcelo Cezário <marcelocezario@gmail.com>
2018-07-05 11:30:27 -0300

    Acertando Views de Estoques (Insumo e Produto)

--- ProducaoAlimentos-Asp.Net/WebApplication1/Models/Estoque.cs
+++ ProducaoAlimentos-Asp.Net/WebApplication1/Models/Estoque.cs
@@ -1,9 +1,15 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public abstract class Estoque
     {
         public int ID { get; set; }
+
+        [Display(Name = "Quantidade em estoque")]
         public double QtdeTotalEstoque { get; set; }
+
+        [Display(Name = "Custo total em estoque")]
         public double CustoTotalEstoque { get; set; }
     }
 }
